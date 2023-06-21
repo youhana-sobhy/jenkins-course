0097b1ba76bfbc86014153e845b99ed3fa12153e
Youhana Sobhy <49269811+youhana-sobhy@users.noreply.github.com>
2023-06-21 23:40:50 +0300

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,18 +1,19 @@
-job('NodeJS example') {
+job('My NodeJS First Job DSL') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/youhana-sobhy/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
-    steps {
-        shell("npm install")
-    }
+	
+	triggers{
+		scm('H/5 * * * *')
+	}
+	wrappers{
+		nodejs('nodejs')
+	}
+	steps{
+		shell('npm install')
+	}
+	
 }
